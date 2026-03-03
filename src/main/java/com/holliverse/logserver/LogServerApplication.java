049f9852dd4d8563fef32b5128d78d84f05b9f94
rettooo <gkdud9236@naver.com>
2026-03-03 22:51:43 +0900

    [HSC-175] fix: 스캔 지우기

--- src/main/java/com/holliverse/logserver/LogServerApplication.java
+++ src/main/java/com/holliverse/logserver/LogServerApplication.java
@@ -2,10 +2,8 @@ package com.holliverse.logserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 public class LogServerApplication {
 
     public static void main(String[] args) {
