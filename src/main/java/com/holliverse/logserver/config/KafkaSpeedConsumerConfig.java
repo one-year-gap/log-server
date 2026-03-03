27617a18d1ba3a8bc343b64b1d97e9e8bde8ef99
rettooo <gkdud9236@naver.com>
2026-03-03 20:37:07 +0900

    [HSC-175] feat: 카프카 Consumer 설정

---
+++ src/main/java/com/holliverse/logserver/config/KafkaSpeedConsumerConfig.java
@@ -0,0 +1,54 @@
+package com.holliverse.logserver.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@Configuration
+@EnableKafka
+public class KafkaSpeedConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}") // 로컬에서 9092 -> 나중에 msk 구성 시, 변경 필요
+    private String bootstrapServers;
+
+    @Value("${app.kafka.groups.speed:speed-layer-group}") // 컨슈머 그룹 이름 
+    private String speedGroupId;
+
+    /* consumer factory bean */
+    // MAX_POLL_RECORDS_CONFIG, 1 (중요): 폴링할때 한건만 가져옴 
+    // ENABLE_AUTO_COMMIT_CONFIG, false: 자동 커밋 비활성화 -> 수동 커밋 필요 -> AckMode.RECORD 사용으로 코드 로직이 성공후 커밋
+    // AUTO_OFFSET_RESET_CONFIG, earliest: 컨슈머 그룹이 처음 생성되었을때, 토픽의 맨 처음 데이터부터 읽어옴 -> 데이터 유실 방지 
+
+    @Bean
+    public ConsumerFactory<String, String> speedConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, speedGroupId);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    /* container Factory bean -> 실제 kafka listener 돌아가는 엔진  */ 
+    // AckMode.RECORD 사용으로 코드 로직이 성공후 커밋 -> 1건 처리하자마자 즉시 커밋 
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> speedLayerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(speedConsumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.RECORD);
+        return factory;
+    }
+}
